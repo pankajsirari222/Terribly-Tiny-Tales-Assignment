354167f2680504df0722a0708e40fbd2db63bc05
Pankaj Singh Sirari <Pankajsirari222@gmail.com>
2023-05-15 13:15:57 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Histogram from "./histogram";
+import { CSVLink } from "react-csv";
+import "./App.css";
 
 function App() {
+  const [ans, setAns] = useState(null);
+  const [flag, setFlag] = useState(false);
+
+  const handleClick = async (e) => {
+    const response = await fetch("https://www.terriblytinytales.com/test.txt", {
+      method: "GET",
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    })
+      .then(async (response) => {
+        const apiData = await response.text();
+
+        const words = apiData.split(" ");
+
+        let freq = {};
+
+        words.forEach((word) => {
+          if (!/^[a-zA-Z0-9]+$/.test(word)) {
+            return;
+          }
+
+          if (freq[word]) {
+            freq[word]++;
+          } else {
+            freq[word] = 1;
+          }
+        });
+
+        let desc = Object.keys(freq).sort((a, b) => freq[b] - freq[a]);
+
+        let result = [];
+
+        for (let i = 0; i < 20 && i < desc.length; i++) {
+          let temp = {};
+          temp["word"] = desc[i];
+          temp["count"] = freq[desc[i]];
+          result.push(temp);
+        }
+
+        setAns(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    setFlag(true);
+  };
+
+  const headers = [
+    { label: "word", key: "word" },
+    { label: "count", key: "count" },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="div-1">
+      <h1> Terribly Tiny Tales Assignment</h1>
+        {flag ? (
+          <Histogram data={ans} />
+        ) : (
+          <div className="submit-btn">
+            <h3> Click Here to fetch the data : </h3>
+            <button type="submit" onClick={handleClick}>
+              Submit
+            </button>
+          </div>
+        )}
+
+        {ans && (
+          <CSVLink
+            data={ans}
+            headers={headers}
+            filename={"words_Frequency_Data.csv"}
+            className="export-text"
+          >
+            Export to CSV
+          </CSVLink>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/histogram.js
@@ -0,0 +1,36 @@
+import React from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from "recharts";
+
+
+
+export default function Histogram({data}) {
+  return (
+    <BarChart
+      width={500}
+      height={300}
+      data={data}
+      margin={{
+        top: 5,
+        right: 30,
+        left: 20,
+        bottom: 5
+      }}
+      barSize={20}
+    >
+      <XAxis dataKey="word" scale="band" padding={{ left: 10, right: 10 }} />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <CartesianGrid strokeDasharray="3 3" />
+      <Bar dataKey="count" fill="#8884d8" background={{ fill: "#eee" }} />
+    </BarChart>
+  );
+}
